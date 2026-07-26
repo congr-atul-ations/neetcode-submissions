fe52df162400ade1ad3a061892c7736904485ee1
Atul Tiwari <contact@atul-tiwari.com>
2026-07-27 05:07:28 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.1
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Atul"
+print(type(variable))
+variable = [1,2,3,4]
+print(type(variable))
